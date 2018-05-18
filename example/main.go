28f15159952c60844193e617712aecf4bcacb554
Michael Persson <michael.ake.persson@gmail.com>
2018-05-18 14:38:46 -0500

    add tls cfg

--- example/main.go
+++ example/main.go
@@ -12,17 +12,18 @@ import (
 
 func main() {
 	server := flag.String("server", "ldap", "LDAP server.")
+	insecure := flag.Bool("insecure", false, "Insecure TLS.")
 	port := flag.Int("port", 636, "LDAP port.")
 	domain := flag.String("domain", "", "AD Domain.")
 	base := flag.String("base", "", "LDAP Base.")
 	flag.Parse()
 
-	t := tlscfg.New(&tlscfg.Options{})
-	if err := t.Init(); err != nil {
+	c := tlscfg.New(&tlscfg.Options{ServerName: *server, Insecure: *insecure})
+	if err := c.Init(); err != nil {
 		log.Fatal(err)
 	}
 
-	_, err := auth.Open("ldap", []string{fmt.Sprintf("%s:%d", *server, *port)}, auth.TLS(t.Config()), auth.Domain(*domain), auth.Base(*base))
+	_, err := auth.Open("ldap", []string{fmt.Sprintf("%s:%d", *server, *port)}, auth.TLS(c.Config()), auth.Domain(*domain), auth.Base(*base))
 	if err != nil {
 		log.Fatal(err)
 	}
